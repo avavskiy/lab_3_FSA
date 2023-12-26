cf93b50e82d1beda74ffae535b27f15f325950ae
avavskiy <andrejmercev@gmail.com>
2023-12-27 01:40:40 +0300

    27.12 2.0

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include "machine.h"
 
 int main() {
-    std::string inputText;
+    std::basic_string<char> inputText;
     std::cout << "Введите строку для проверки: ";
     std::getline(std::cin, inputText);
 
-    FSA machine;
+    FSA<char> machine;
     machine.answer(inputText);
 
     return 0;
